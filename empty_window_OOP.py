6756220ca919d6ffb955df4f72b3536fdb9a8771
Maksim <semimaks@mail.ru>
2021-09-14 15:37:43 +0300

    Работа с аргументом 'side='

--- empty_window_OOP.py
+++ empty_window_OOP.py
@@ -8,11 +8,22 @@ class MyGUI:
         # Создаём элемент интерфейса главного окна.
         self.main_window = tkinter.Tk()
 
-        # Создаём элемент интерфейса Label.
-        self.label = tkinter.Label(self.main_window, text='Новое окно')
+        # Создаём элемент интерфейса Label,
+        # элементы распологаются друг под другом.
+        self.label1 = tkinter.Label(self.main_window, text='Первое окно')
+        self.label2 = tkinter.Label(self.main_window, text='Второе окно')
+
+        # Создаём элемент интерфейса Label с аргументом side='left',
+        # элементы распологаются друг возле друга в родительском элементе
+        # интерфейса максимально слева. Ещё есть 'right', 'top' и 'bottom'.        self.label3 = tkinter.Label(self.main_window, text='Третье окно')
+        self.label3 = tkinter.Label(self.main_window, text='Третье окно')
+        self.label4 = tkinter.Label(self.main_window, text='Четвёртое окно')
 
         # Вызываем метод pack элемента label.
-        self.label.pack()
+        self.label1.pack()
+        self.label2.pack()
+        self.label3.pack(side='left')
+        self.label4.pack(side='left')
 
         # Входим в главный цикл tkinter.
         tkinter.mainloop()
