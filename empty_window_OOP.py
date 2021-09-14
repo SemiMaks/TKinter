424ea0f5ce0921550ceb10e3b5089168a90f6b40
Maksim <semimaks@mail.ru>
2021-09-14 15:27:17 +0300

    Работаем в стиле ООП.Добавлен элемент интерфейса Label

--- empty_window_OOP.py
+++ empty_window_OOP.py
@@ -8,6 +8,12 @@ class MyGUI:
         # Создаём элемент интерфейса главного окна.
         self.main_window = tkinter.Tk()
 
+        # Создаём элемент интерфейса Label.
+        self.label = tkinter.Label(self.main_window, text='Новое окно')
+
+        # Вызываем метод pack элемента label.
+        self.label.pack()
+
         # Входим в главный цикл tkinter.
         tkinter.mainloop()
 
